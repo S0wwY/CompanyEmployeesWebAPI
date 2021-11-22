248f3ea667585aeeaaf194288be195eaba726728
Владислав <s0wwy@mail.ru>
2021-11-23 00:12:39 +0300

    Creating resources

--- CompanyEmployees1/Mapping/MappingProfile.cs
+++ CompanyEmployees1/Mapping/MappingProfile.cs
@@ -18,6 +18,8 @@ namespace CompanyEmployees1.Mapping
             opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
+            CreateMap<CompanyForCreationDto, Company>();
+            CreateMap<EmployeeForCreationDto, Employee>();
         }
     }
 }
