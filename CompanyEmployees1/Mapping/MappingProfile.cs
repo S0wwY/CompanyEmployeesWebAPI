ba50836a8a3d4b1289b1f24e7e06a4ef7ff53332
Владислав <s0wwy@mail.ru>
2021-11-28 21:53:26 +0300

    Add PutRequest

--- CompanyEmployees1/Mapping/MappingProfile.cs
+++ CompanyEmployees1/Mapping/MappingProfile.cs
@@ -18,8 +18,12 @@ namespace CompanyEmployees1.Mapping
             opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
+
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+
+            CreateMap<EmployeeForUpdateDto, Employee>();
+            CreateMap<CompanyForUpdateDto, Company>();
         }
     }
 }
